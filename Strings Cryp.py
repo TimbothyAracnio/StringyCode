4f7942e509977bd14e2445bbc943acae3c8226ad
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-10-24 15:20:10 -0500

    Added added names!

---
+++ Strings Cryp.py
@@ -0,0 +1,10 @@
+# Strings
+
+# We will be talking about Concatenation
+#    2 or more strings put'n them together
+
+firstName = "Doug"
+lastName = "Dimmadome"
+
+# stick'n them together
+print(firstName + " " + lastName)
