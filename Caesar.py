50c93b953f74de4fbf88fbf46155a790eefe8b7d
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-10-31 16:00:08 -0500

    trying to make work

--- Caesar.py
+++ Caesar.py
@@ -1,21 +1,21 @@
-def letterToIndex():
+def letterToIndex(letter, key):
     from string import ascii_lowercase
     alphabet = ascii_lowercase + ' '
 
-    strg = input("What is your text: ")
-    key = input("What is your shift: ")
+    for i in range(0, len(letter) + 1):
+        idx = alphabet.find(letter) + key
 
-    for i in range(0, len(strg) + 1):
-        singstrg = strg[i]
-        idx = alphabet.find() + key
 
         if idx > 26:
             idx = idx - 26
-        let = ascii_lowercase[idx]
+
+
+
+    let = ascii_lowercase[idx]
 
 
     return let
 
 
 
-print(letterToIndex())
+print(letterToIndex("y", 4))
